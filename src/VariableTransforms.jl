fd561ce7b139ad804eefad06f484fcec566394be
Marcin Zamojski <marcin.zamojski@gmail.com>
2021-02-25 22:56:40 +0100

    *Add derivatives

--- src/VariableTransforms.jl
+++ src/VariableTransforms.jl
@@ -22,4 +22,14 @@ module VariableTransforms
     from_pos_to_R(x)=log(x)
     from_R_to_pos(x)=exp(x)
 
+    from_11_to_R_prime(x)=2/(1-x^2)
+    from_01_to_R_prime(x)=1/(x-x^2)
+    from_0L_to_R_prime(x; fmax=1, steepness=0.1, midpoint=0)=(1/x + 1/(fmax-x))/steepness
+    from_pos_to_R_prime(x)=1/x
+
+    from_R_to_11_prime(x)=2*from_R_to_01(x)*(1-from_R_to_01(x))
+    from_R_to_01_prime(x)=from_R_to_01(x)*(1-from_R_to_01(x))
+    from_R_to_pos_prime(x)=from_R_to_pos(x)
+    from_R_to_0L_prime(x; fmax=1, steepness=0.1, midpoint=0)=fmax*steepness*from_R_to_0L(x; fmax=1, steepness=steepness, midpoint=midpoint)*(1-from_R_to_0L(x; fmax=1, steepness=steepness, midpoint=midpoint))
 end # module
+
